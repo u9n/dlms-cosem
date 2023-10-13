24c06fdc7df572de5bbc42747e0401052088897b
Isidro <isidroariass@hotmail.es>
2023-10-13 08:24:26 +0200

    set with block (#1)
    * implemented parsing
    
    * fixed serialization
    
    * fix factory
    
    * tests for set response with block
    
    * implemented
    
    * set response with last block
    
    * factory
    
    ---------
    
    Co-authored-by: Isidro Arias <isidro.arias@wslw.es>

--- dlms_cosem/protocol/xdlms/set.py
+++ dlms_cosem/protocol/xdlms/set.py
@@ -6,6 +6,10 @@ from dlms_cosem import cosem
 from dlms_cosem import enumerations as enums
 from dlms_cosem.protocol.xdlms.base import AbstractXDlmsApdu
 from dlms_cosem.protocol.xdlms.invoke_id_and_priority import InvokeIdAndPriority
+from dlms_cosem.dlms_data import (
+    decode_variable_integer,
+    encode_variable_integer,
+)
 
 """
 Set-Request ::= CHOICE
@@ -100,7 +104,7 @@ class SetRequestNormal(AbstractXDlmsApdu):
 
 
 @attr.s(auto_attribs=True)
-class SetRequestWithFirstBlock:
+class SetRequestWithFirstBlock(AbstractXDlmsApdu):
     """
     Set-Request-With-First-Datablock ::= SEQUENCE
     {
@@ -118,11 +122,92 @@ class SetRequestWithFirstBlock:
     }
     """
 
-    ...
+    TAG: ClassVar[int] = 193
+    RESPONSE_TYPE: ClassVar[enums.SetRequestType] = enums.SetRequestType.WITH_FIRST_BLOCK
+    cosem_attribute: cosem.CosemAttribute = attr.ib(
+        validator=attr.validators.instance_of(cosem.CosemAttribute)
+    )
+    data: bytes = attr.ib(validator=attr.validators.instance_of(bytes))
+    access_selection: Optional[Any] = attr.ib(default=None)
+    invoke_id_and_priority: InvokeIdAndPriority = attr.ib(
+        factory=InvokeIdAndPriority,
+        validator=attr.validators.instance_of(InvokeIdAndPriority),
+    )
+
+    @classmethod
+    def from_bytes(cls, source_bytes: bytes):
+        data = bytearray(source_bytes)
+        tag = data.pop(0)
+        if tag != cls.TAG:
+            raise ValueError(
+                f"Tag for SetRequest is not correct. Got {tag}, should be {cls.TAG}"
+            )
+
+        type_choice = enums.SetRequestType(data.pop(0))
+        if type_choice is not enums.SetRequestType.WITH_FIRST_BLOCK:
+            raise ValueError("The type of the SetRequest is not for a SetRequestWithFirstBlock")
+
+        invoke_id_and_priority = InvokeIdAndPriority.from_bytes(
+            data.pop(0).to_bytes(1, "big")
+        )
+        cosem_attribute = cosem.CosemAttribute.from_bytes(data[:9])
+        data = data[9:]
+
+        has_access_selection = bool(data.pop(0))
+        if has_access_selection:
+            raise NotImplementedError("Selective access on SET is not implemented")
+        else:
+            access_selection = None
+
+        last_block = bool(data.pop(0))
+        if last_block:
+            raise ValueError(
+                f"Last block set to true in a SetRequestWithFirstBlock. Should only be set "
+                f"for a SetRequestWithBlock"
+            )
+
+        block_number = int.from_bytes(data[:4], "big")
+        if block_number !=1:
+            raise ValueError(
+                "block_number should be 1 in a SetRequestWithFirstBlock. "
+                f"Instead received {block_number}"
+            )
+
+        data = data[4:]
+
+        data_length, data = decode_variable_integer(data)
+        if data_length != len(data):
+            raise ValueError(
+                "The octet string in block data is not of the correct length"
+            )
+
+        return cls(
+            cosem_attribute=cosem_attribute,
+            data=bytes(data),
+            access_selection=access_selection,
+            invoke_id_and_priority=invoke_id_and_priority,
+        )
+
+    def to_bytes(self) -> bytes:
+        out = bytearray()
+        out.append(self.TAG)
+        out.append(self.RESPONSE_TYPE.value)
+        out.extend(self.invoke_id_and_priority.to_bytes())
+        out.extend(self.cosem_attribute.to_bytes())
+        if self.access_selection:
+            out.extend(b"\x01")
+            out.extend(self.access_selection.to_bytes())
+        else:
+            out.extend(b"\x00")
+        out.append(0) # last_block
+        out.extend(b'\x00\x00\x00\x01') # block number
+        out.extend(encode_variable_integer(len(self.data)))
+        out.extend(self.data)
+        return bytes(out)
 
 
 @attr.s(auto_attribs=True)
-class SetRequestWithBlock:
+class SetRequestWithBlock(AbstractXDlmsApdu):
     """
     Set-Request-With-Datablock ::= SEQUENCE
     {
@@ -142,7 +227,7 @@ class SetRequestWithBlock:
 
 
 @attr.s(auto_attribs=True)
-class SetRequestWithList:
+class SetRequestWithList(AbstractXDlmsApdu):
     """
     Set-Request-With-List ::= SEQUENCE
     {
@@ -162,7 +247,7 @@ class SetRequestWithList:
 
 
 @attr.s(auto_attribs=True)
-class SetRequestWithListFirstBlock:
+class SetRequestWithListFirstBlock(AbstractXDlmsApdu):
     """
     Set-Request-With-List-And-First-Datablock ::= SEQUENCE
     {
@@ -209,9 +294,12 @@ class SetRequestFactory:
         request_type = enums.SetRequestType(data.pop(0))
         if request_type == enums.SetRequestType.NORMAL:
             return SetRequestNormal.from_bytes(source_bytes)
-
+        elif request_type == enums.SetRequestType.WITH_FIRST_BLOCK:
+            return SetRequestWithFirstBlock.from_bytes(source_bytes)
+        elif request_type == enums.SetRequestType.WITH_BLOCK:
+            return SetRequestWithBlock.from_bytes(source_bytes)
         else:
-            raise NotImplementedError("Only SetRequestNormal implemented")
+            raise NotImplementedError("Unsupported set subtype: {request_type}")
 
 
 @attr.s(auto_attribs=True)
@@ -267,7 +355,7 @@ class SetResponseNormal(AbstractXDlmsApdu):
 
 
 @attr.s(auto_attribs=True)
-class SetResponseWithBlock:
+class SetResponseWithBlock(AbstractXDlmsApdu):
     """
     Set-Response-Datablock ::= SEQUENCE
     {
@@ -276,11 +364,48 @@ class SetResponseWithBlock:
     }
     """
 
-    ...
+    TAG: ClassVar[int] = 197
+    RESPONSE_TYPE: ClassVar[enums.SetResponseType] = enums.SetResponseType.WITH_BLOCK
+    invoke_id_and_priority: InvokeIdAndPriority = attr.ib(
+        factory=InvokeIdAndPriority,
+        validator=attr.validators.instance_of(InvokeIdAndPriority),
+    )
+    block_number: int = attr.ib(validator=attr.validators.instance_of(int), default=0)
+
+    @classmethod
+    def from_bytes(cls, source_bytes: bytes):
+        data = bytearray(source_bytes)
+        tag = data.pop(0)
+        if tag != cls.TAG:
+            raise ValueError(
+                f"Tag for SetResponse is not correct. Got {tag}, should be {cls.TAG}"
+            )
+
+        type_choice = enums.SetResponseType(data.pop(0))
+        if type_choice is not enums.SetResponseType.WITH_BLOCK:
+            raise ValueError(
+                "The type of the SetResponse is not for a SetResponseWithBlock"
+            )
+
+        invoke_id_and_priority = InvokeIdAndPriority.from_bytes(
+            data.pop(0).to_bytes(1, "big")
+        )
+
+        block_number = int.from_bytes(data[:4], "big")
+
+        return cls(invoke_id_and_priority=invoke_id_and_priority, block_number=block_number)
+
+    def to_bytes(self) -> bytes:
+        out = bytearray()
+        out.append(self.TAG)
+        out.append(self.RESPONSE_TYPE.value)
+        out.extend(self.invoke_id_and_priority.to_bytes())
+        out.extend(self.block_number.to_bytes(4, 'big'))
+        return bytes(out)
 
 
 @attr.s(auto_attribs=True)
-class SetResponseLastBlock:
+class SetResponseLastBlock(AbstractXDlmsApdu):
     """
     Set-Response-Last-Datablock ::= SEQUENCE
     {
@@ -290,11 +415,53 @@ class SetResponseLastBlock:
     }
     """
 
-    ...
+    TAG: ClassVar[int] = 197
+    RESPONSE_TYPE: ClassVar[enums.SetResponseType] = enums.SetResponseType.WITH_LAST_BLOCK
+    result: enums.DataAccessResult = attr.ib(
+        validator=attr.validators.instance_of(enums.DataAccessResult)
+    )
+    invoke_id_and_priority: InvokeIdAndPriority = attr.ib(
+        factory=InvokeIdAndPriority,
+        validator=attr.validators.instance_of(InvokeIdAndPriority),
+    )
+    block_number: int = attr.ib(validator=attr.validators.instance_of(int), default=0)
+
+    @classmethod
+    def from_bytes(cls, source_bytes: bytes):
+        data = bytearray(source_bytes)
+        tag = data.pop(0)
+        if tag != cls.TAG:
+            raise ValueError(
+                f"Tag for SetResponse is not correct. Got {tag}, should be {cls.TAG}"
+            )
+
+        type_choice = enums.SetResponseType(data.pop(0))
+        if type_choice is not enums.SetResponseType.WITH_LAST_BLOCK:
+            raise ValueError(
+                "The type of the SetResponse is not for a SetResponseLastBlock"
+            )
+
+        invoke_id_and_priority = InvokeIdAndPriority.from_bytes(
+            data.pop(0).to_bytes(1, "big")
+        )
+
+        result = enums.DataAccessResult(data.pop(0))
+        block_number = int.from_bytes(data[:4], "big")
+
+        return cls(result=result, invoke_id_and_priority=invoke_id_and_priority, block_number=block_number)
+
+    def to_bytes(self) -> bytes:
+        out = bytearray()
+        out.append(self.TAG)
+        out.append(self.RESPONSE_TYPE.value)
+        out.extend(self.invoke_id_and_priority.to_bytes())
+        out.append(self.result.value)
+        out.extend(self.block_number.to_bytes(4, 'big'))
+        return bytes(out)
 
 
 @attr.s(auto_attribs=True)
-class SetResponseLastBlockWithList:
+class SetResponseLastBlockWithList(AbstractXDlmsApdu):
     """
     Set-Response-Last-Datablock-With-List ::= SEQUENCE
     {
@@ -308,7 +475,7 @@ class SetResponseLastBlockWithList:
 
 
 @attr.s(auto_attribs=True)
-class SetResponseWithList:
+class SetResponseWithList(AbstractXDlmsApdu):
     """
     Set-Response-With-List ::= SEQUENCE
     {
@@ -340,6 +507,9 @@ class SetResponseFactory:
         request_type = enums.SetResponseType(data.pop(0))
         if request_type == enums.SetResponseType.NORMAL:
             return SetResponseNormal.from_bytes(source_bytes)
-
+        elif request_type == enums.SetResponseType.WITH_BLOCK:
+            return SetResponseWithBlock.from_bytes(source_bytes)
+        elif request_type == enums.SetResponseType.WITH_LAST_BLOCK:
+            return SetResponseLastBlock.from_bytes(source_bytes)
         else:
-            raise NotImplementedError("Only SetResponseNormal implemented")
+            raise NotImplementedError(f"not implemented {request_type}")
--- tests/test_xdlms/test_set.py
+++ tests/test_xdlms/test_set.py
@@ -28,7 +28,6 @@ class TestSetRequestNormal:
             xdlms.SetRequestNormal.from_bytes(data)
 
     def test_wrong_type_raises_value_error(self):
-
         data = b"\xc1\x02\xc1\x00\x08\x00\x00\x01\x00\x00\xff\x02\x00\t\x0c\x07\xe5\x01\x18\xff\x0e09P\xff\xc4\x00"
         with pytest.raises(ValueError):
             xdlms.SetRequestNormal.from_bytes(data)
@@ -45,12 +44,21 @@ class TestSetRequestFactory:
         with pytest.raises(ValueError):
             xdlms.SetRequestFactory.from_bytes(data)
 
-    def test_request_with_first_block_raises_not_implemented_error(self):
-        data = b"\xc1\x02\xc1\x00\x08\x00\x00\x01\x00\x00\xff\x02\x00\t\x0c\x07\xe5\x01\x18\xff\x0e09P\xff\xc4\x00"
-        with pytest.raises(NotImplementedError):
-            xdlms.SetRequestFactory.from_bytes(data)
+    def test_request_with_first_block(self):
+        # example from Table 137 – Example: Writing the value of a single attribute with block transfer
+        data = bytes.fromhex(
+            "C102C1"
+            "00010000800000FF0200"
+            "00"
+            "00000001"
+            "15"
+            "09320102030405060708091011121314"
+            "1516171819"
+        )
+        request = xdlms.SetRequestFactory.from_bytes(data)
+        assert isinstance(request, xdlms.SetRequestWithFirstBlock)
 
-    def test_set_request_with_block_raises_not_implemented_error(self):
+    def test_set_request_with_block(self):
         data = b"\xc1\x03\xc1\x00\x08\x00\x00\x01\x00\x00\xff\x02\x00\t\x0c\x07\xe5\x01\x18\xff\x0e09P\xff\xc4\x00"
         with pytest.raises(NotImplementedError):
             xdlms.SetRequestFactory.from_bytes(data)
@@ -100,15 +108,15 @@ class TestSetResponseFactory:
         with pytest.raises(ValueError):
             xdlms.SetResponseFactory.from_bytes(data)
 
-    def test_set_response_with_block_raises_not_implemented_error(self):
+    def test_set_response_with_block(self):
         data = b"\xc5\x02\xc1\x00"
-        with pytest.raises(NotImplementedError):
-            xdlms.SetResponseFactory.from_bytes(data)
+        request = xdlms.SetResponseFactory.from_bytes(data)
+        assert isinstance(request, xdlms.SetResponseWithBlock)
 
-    def test_set_response_last_block_raises_not_implemented_error(self):
+    def test_set_response_last_block(self):
         data = b"\xc5\x03\xc1\x00"
-        with pytest.raises(NotImplementedError):
-            xdlms.SetResponseFactory.from_bytes(data)
+        request = xdlms.SetResponseFactory.from_bytes(data)
+        assert isinstance(request, xdlms.SetResponseLastBlock)
 
     def test_set_response_last_block_with_list_raises_not_implemented_error(self):
         data = b"\xc5\x04\xc1\x00"
@@ -119,3 +127,60 @@ class TestSetResponseFactory:
         data = b"\xc5\x05\xc1\x00"
         with pytest.raises(NotImplementedError):
             xdlms.SetResponseFactory.from_bytes(data)
+
+
+class TestSetRequestWithFirstBlock:
+    def test_transform_bytes(self):
+        data = bytes.fromhex(
+            "C102C1"
+            "00010000800000FF0200"
+            "00"
+            "00000001"
+            "15"
+            "09320102030405060708091011121314"
+            "1516171819"
+        )
+
+        request = xdlms.SetRequestWithFirstBlock(
+            cosem_attribute=cosem.CosemAttribute(
+                interface=enumerations.CosemInterface.DATA,
+                instance=cosem.Obis(a=0, b=0, c=128, d=0, e=0, f=255),
+                attribute=2,
+            ),
+            data=bytes.fromhex("093201020304050607080910111213141516171819"),
+            access_selection=None,
+            invoke_id_and_priority=xdlms.InvokeIdAndPriority(
+                invoke_id=1, confirmed=True, high_priority=True
+            ),
+        )
+        assert data == request.to_bytes()
+        assert request == xdlms.SetRequestWithFirstBlock.from_bytes(data)
+
+
+class TestSetResponseWithBlock:
+    def test_transform_bytes(self):
+        data = bytes.fromhex("C502C1" "00000001")
+
+        response = xdlms.SetResponseWithBlock(
+            invoke_id_and_priority=xdlms.InvokeIdAndPriority(
+                invoke_id=1, confirmed=True, high_priority=True
+            ),
+            block_number=1,
+        )
+        assert data == response.to_bytes()
+        assert response == xdlms.SetResponseWithBlock.from_bytes(data)
+
+
+class TestSetResponseLastBlock:
+    def test_transform_bytes(self):
+        data = bytes.fromhex("C503C10000000002")
+
+        response = xdlms.SetResponseLastBlock(
+            invoke_id_and_priority=xdlms.InvokeIdAndPriority(
+                invoke_id=1, confirmed=True, high_priority=True
+            ),
+            result=enumerations.DataAccessResult.SUCCESS,
+            block_number=2,
+        )
+        assert data == response.to_bytes()
+        assert response == xdlms.SetResponseLastBlock.from_bytes(data)
